bc02a1de1f7f0518e6a40ef50b7e968cb9ddcd83
Remo Cocco <remococco@gmail.com>
2011-01-23 21:30:30 -0500

    first commit

---
+++ src/flashflashrevolutionbot/Main.java
@@ -0,0 +1,15 @@
+package flashflashrevolutionbot;
+
+/**
+ * Main class for Flash Flash Revolution Robot
+ */
+public class Main {
+    /**
+     * Main Methods
+     * This will set up the application and run it
+     */
+    public static void main (String... args) {
+        System.out.println("Hello Flash Flash!");
+    }
+}
+
---
+++ test/robotcapabilitiestest/ScreenCaptureCapabilitiesTest.java
@@ -0,0 +1,14 @@
+package robotcapabilitiestest;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class ScreenCaptureCapabilitiesTest {
+    @Test
+    public void helloWorldTest() {
+        System.out.println("Hello World!");
+        assertTrue(true);
+    }
+
+}
+
